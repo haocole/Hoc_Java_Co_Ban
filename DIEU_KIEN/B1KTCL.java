9a53c279247a6aa598e228d80c2e13303324dd38
haocole <tbysnanyoel@gmail.com>
2026-04-06 13:50:01 +0700

    Thêm bai tập chương trình Điều Kiện

---
+++ DIEU_KIEN/B1KTCL.java
@@ -0,0 +1,21 @@
+package DIEU_KIEN;
+import java.util.Scanner;
+
+public class B1KTCL {
+    public static void main(String[] args) {
+        Scanner nhap = new Scanner(System.in);
+
+        System.out.print("Nhap so nguyen: ");
+        int so = nhap.nextInt();
+
+        if (so % 2 == 0){
+            System.out.print(so +  "La so chan");
+        }
+        else {
+            System.out.print(so + "La so le");
+        }
+
+nhap.close();
+    }
+    
+}
